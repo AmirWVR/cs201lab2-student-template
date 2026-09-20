2dc42ea1ba2e6a04ac3a080bcf9e1966d44ad40d
AmirWVR <kasweaver35@gmail.com>
2026-09-20 17:38:37 +0800

    FINALLY. FIRST ITERATION OF Q1.

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -101,8 +101,108 @@ public class SinglyLinkedList<E extends Comparable<E>> {
 
     // write your codes here
     public void swap(){
-        
+        //get the elements of the linked list first
+        int listSize = size();
 
+        // number of loop iteartions
+        int iterations;
+        if(listSize % 2 == 0){
+            iterations = listSize / 2;
+        } else {
+            iterations = (listSize - 1 ) / 2;
+        }
+
+        // each iteration counts as 1 swap
+        
+        E prevMinValue = null;
+        E prevMaxValue = null;
+
+        for (int i = 0; i < iterations; i++) {
+            Node<E> minNode = null;
+            Node<E> prevOfMin = null;
+            Node<E> maxNode = null;
+            Node<E> prevOfMax = null;
+            Node<E> prevOfCurrent = null;
+            Node<E> current = head;
+
+            while (current != null) {
+                boolean inRange = (i == 0) ||
+                    (current.getElement().compareTo(prevMinValue) > 0 &&
+                    current.getElement().compareTo(prevMaxValue) < 0);
+
+                if (inRange) {
+                    if (minNode == null || current.getElement().compareTo(minNode.getElement()) < 0) {
+                        minNode = current;
+                        prevOfMin = prevOfCurrent;
+                    }
+                    if (maxNode == null || current.getElement().compareTo(maxNode.getElement()) > 0) {
+                        maxNode = current;
+                        prevOfMax = prevOfCurrent;
+                    }
+                }
+                prevOfCurrent = current;
+                current = current.next;
+            }
+
+            // swap nodes
+            if (minNode == maxNode){
+                break; 
+            }
+
+            if (minNode.next == maxNode){
+                Node<E> nextOfMax = maxNode.next;
+                if (prevOfMin == null)
+                {
+                    head = maxNode;
+                }
+                else
+                {
+                    prevOfMin.next = maxNode;
+                }
+                maxNode.next = minNode;
+                minNode.next = nextOfMax;
+            }
+            else if (maxNode.next == minNode){
+                Node<E> nextOfMin = minNode.next;
+                if (prevOfMax == null)
+                {
+                    head = minNode;
+                }
+                else
+                {
+                    prevOfMax.next = minNode;
+                }
+                minNode.next = maxNode;
+                maxNode.next = nextOfMin;
+            } else {
+                if (prevOfMin == null) {
+                    head = maxNode;
+                } else {
+                    prevOfMin.next = maxNode;
+                }
+
+                if (prevOfMax == null) {
+                    head = minNode;
+                } else {
+                    prevOfMax.next = minNode;
+                }
+
+                Node<E> nextOfMax = maxNode.next;
+                Node<E> nextOfMin = minNode.next;
+                maxNode.next = nextOfMin;
+                minNode.next = nextOfMax;
+            }
+
+            prevMinValue = minNode.getElement();
+            prevMaxValue = maxNode.getElement();
+        }
+
+        Node<E> newTail = head;
+
+        while(newTail.next != null){
+            newTail = newTail.next;
+        }
+        tail = newTail;
     }
    
 }
